6ecd282c88634ae3fbad96d6d2f3b32301ff60bf
tejas1598 <tejas.anandas@nmiet.edu.in>
2026-01-18 17:13:45 +0530

    Initial setup: Spring Boot + Postgres Config

---
+++ src/main/java/com/cravefeed/backend/CraveFeedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cravefeed.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraveFeedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraveFeedBackendApplication.class, args);
+	}
+
+}
